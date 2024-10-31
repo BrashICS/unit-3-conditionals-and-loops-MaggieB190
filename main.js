a7eb60dc75d99f0941bb2d2945b9ceaef71394d0
Maggie Boyd <maggie.boyd@stu.ocsb.ca>
2024-10-31 11:14:25 -0400

    Spooky adventure game almost halfway

--- main.js
+++ main.js
@@ -73,3 +73,64 @@ function ask_name() {
 }
 
 //Part 2
+
+//Halloween Assignment
+
+function start_game() {
+    let name = prompt("What is you name?");
+
+    alert(`Welcome ${name} to Haunted St. Matthew High School! I hope you're ready for a terrifing experience...`);
+    alert(`School has just ended, your friend asks you if you want to go see Mr. Brash with them.`);
+
+    let brash_house = prompt(`Do you (1) go with them to see Mr. Brash or (2) just go home?`);
+
+    if (brash_house == 1) {
+        go_brash_room()
+    }
+
+    if (brash_house == 2) {
+        go_home_first()
+    }
+}
+
+function go_brash_room() {
+    alert(`You and your friend hear loud noises from Mr. Brash's room. It sounds like the smashing of legos?...`);
+    let noise_chicken = prompt(`Do you (1) investigate or (2) chicken out?`);
+
+    if (noise_chicken == 1) {
+        investigate_noise()
+    }
+
+    if (noise_chicken == 2) {
+        go_home_second()
+    }
+
+}
+
+function investigate_noise() {
+    alert(`You and your friend enter Mr. Brash's room. You see him unsteadily walking around his room and smashing his electronics!`);
+    alert(`Your friend yells: "Mr. Brash! What are you doing??"`);
+    alert(`Mr. Brash flips around. His eyes are dark, his veins are rough and coursing... Almost like an infection...`);
+    alert(`There's no doubt about it, he's zombified!`);
+    let weapon = prompt(`Mr. Brash runs at you to attack. What do you grab to protect yourself? (1) His laptop or (2) his R2-D2 mug?`);
+
+    if (weapon == 2) {
+        mug()
+    }
+}
+
+function mug() {
+    alert(`You smash Mr. Brash's R2-D2 mug on his head and it's super effective!`);
+    alert(`Mr. Brash collapses on the floor.`);
+    alert(`You and your friend flip some tables and stack them ontop. You've won! But now what?...`);
+    alert(`You turn and investgate the electronic Zombie-Brash has smashed. You wonder how he could've gotten zombified...`);
+    ///COntinue to have him mumble abt gitbhub, solve his issue story done, brash no zombie
+}
+
+function go_home_first() {
+    alert(`You arrive at home and start your usual after school routine.`);
+}
+
+function go_home_second() {
+    alert(``)
+}

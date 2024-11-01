1f74d81a865df004857929d83207ee8d73706789
Maggie Boyd <maggie.boyd@stu.ocsb.ca>
2024-11-01 12:00:01 -0400

    only left to do is laptop weapon

--- main.js
+++ main.js
@@ -14,7 +14,6 @@
 /*** Event Listeners ***/
 
 
-
 /*** Functions ***/
 
 // A very accurate rounding function
@@ -109,10 +108,13 @@ function go_brash_room() {
 
 function investigate_noise() {
     alert(`You and your friend enter Mr. Brash's room. You see him unsteadily walking around his room and smashing his electronics!`);
-    alert(`Your friend yells: "Mr. Brash! What are you doing??"`);
-    alert(`Mr. Brash flips around. His eyes are dark, his veins are rough and coursing... Almost like an infection...`);
+    alert(`Your friend yells: "Mr. Brash! What are you doing??" Mr. Brash flips around. His eyes are pitch black, he seems to be dripping something?... Almost like an infection...`);
     alert(`There's no doubt about it, he's zombified!`);
     let weapon = prompt(`Mr. Brash runs at you to attack. What do you grab to protect yourself? (1) His laptop or (2) his R2-D2 mug?`);
+
+    if (weapon == 2) {
+        laptop()
+    }
 
     if (weapon == 2) {
         mug()
@@ -120,18 +122,60 @@ function investigate_noise() {
 }
 
 function mug() {
-    alert(`You smash Mr. Brash's R2-D2 mug on his head and it's super effective!`);
-    alert(`Mr. Brash collapses on the floor.`);
-    alert(`You and your friend flip some tables and stack them ontop. You've won! But now what?...`);
-    ///change that weird line^^^
-    alert(`You turn and investgate the electronic Zombie-Brash has smashed. You wonder how he could've gotten zombified...`);
-    ///COntinue to have him mumble abt gitbhub, solve his issue story done, brash no zombie
+    alert(`You smash Mr. Brash's R2-D2 mug on his head and it's super effective! He collapses on the floor. You've won!`);
+    alert(`You turn and investgate the electronics Zombie-Brash has smashed. You wonder how he could've gotten zombified... The broken screens of his chromebooks are all static with some sort of substance leaking out of them. You try to tap a few keys on one of the better-off chromebooks. Nothing.`);
+    alert(`"Hey, what's that?" asks your friend as they point to a screen. "It almost looks like... Github?..."`);
+    alert(`"G-github.. m-mer-merge..." mumbles Mr. Brash, still collpased on the floor.`);
+    alert(`"It seems like Mr. Brash was dealing with a merge conflict. I bet I could help him." You walk over to Mr. Brash's desk and start your work. You're extremely lucky he was still logged in. There are remnants on his desk of the mysterious substance found on Mr. Brash and the chromebooks, you're careful to avoid it. "Maybe it was the liquid that infected him while he was solving his merge conflict..."`);
+    alert(`"It takes a while, but finally! Merge conflict solved! As you step away from the laptop, you hear bubbling and gurgling... "It's the mysterious substance! It's evaporating!" your friend yells. "Mr. Brash is de-zombiefying!" A few minutes later and Mr. Brash is completely back to normal, eyes and all. He's extremly grateful for all your help and offers to give you extra credit!`);
+    alert(`It seems like Github won't be getting the best of you anytime soon...`);
+    
+    game_over_live()
+}
+
+function laptop() {
+
 }
 
 function go_home_first() {
-    alert(`You arrive at home and start your usual after school routine.`);
+    alert(`You arrive at home and start your usual after school routine. You feel a bit bad for not going to see Mr. Brash with your friend.`);
+    alert(`A few hours later you hear loud raps on your front-door... Who could it be?`);
+    alert(`You open the door and it's your friend! But something seems off... They're dripping? But it's not water... Next thing you know you're on the ground fighting off your best-friend. "M-merge... M-m-erge..." mumbles your friend with pitch black eyes as they pin you to the ground.`);
+    alert(`There's a metallic taste in your mouth and your body starts to burn. You become weaker and weaker with each second that goes by...`);
+    alert(`It's no use fighting... Your body becomes something that's not of your own... The infection taking over... Your eyes are forced to black and the last thing you see is the faint outline of the Github logo... They have won.`);
+
+    game_over_die()
 }
 
 function go_home_second() {
-    alert(``)
+    alert(`You chickened out and went home. BORING.`);
+    alert(`You start your computer science work for the night and spend hours on your project. You finish for the night and go to commit your changes. You see the worst has happened...`);
+    alert(`A merge conflict.`);
+    alert(`You try to fix your mistakes but it's no use. All of a sudden your computer sparks, the screen is filled with errors and a mysterious liquid explodes from the inside. The substance seeps into your skin and immediately it burns. You feel yourself losing movement abilities. Your vision goes black... It's over. Github has won.`);
+
+    game_over_die()
+}
+
+function game_over_live() {
+    let live_new_game_exit = prompt(`Congratulations! You've survived this night at Haunted St. Matthew High School! Would you like to (1) try again or (2) exit?`);
+
+    if (live_new_game_exit == 1) {
+        start_game()
+    }
+
+    if (live_new_game_exit == 2) {
+        alert(`Thank you for playing. Have a spooky Halloween...`);
+    }
+}
+
+function game_over_die() {
+    let die_new_game_exit = prompt(`Unfortunatly, you've lost. The infection has won. Would you like to (1) try again or (2) exit?`);
+
+    if (die_new_game_exit == 1) {
+        start_game()
+    }
+
+    if (die_new_game_exit == 2) {
+        alert(`Thank you for playing. Have a spooky Halloween...`);
+    }
 }
